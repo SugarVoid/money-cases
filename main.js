56eb631042ba17b63a93bbe64ecd94cf82c127b9
Devonte Durham <durhamd@fvtc.edu>
2020-11-23 07:01:04 -0600

    added stuff

--- main.js
+++ main.js
@@ -1,3 +1,29 @@
+class Game {
+    constructor() { }
+
+    playerName = ''
+    activeGame = true
+    playerWinnings = 0
+    bankerOffer = 0
+    round = 0
+}
+
+class Case {
+    constructor() { }
+
+}
+
+const game = {
+    activeBoard: true,
+    playerName: 'Player',
+    playerWinnings: 0,
+    offerValue: 0,
+    round: 0,
+    casesToOpen: 26,
+    prizeValues: [.01, 1, 5, 10, 50, 100, 250, 500, 750, 1000, 3000, 5000, 10000, 15000, 25000, 50000, 75000, 100000, 250000, 500000],
+    selectedValues: [0],
+}
+
 const lbl1 = document.getElementById('1case')
 
 
@@ -8,4 +34,23 @@ function toggleLabel() {
 
 function Simp() {
     console.log('test')
+}
+
+function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+
+    return array;
 }
