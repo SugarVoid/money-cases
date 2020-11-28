c40b07ca9e407779939f1b4896dad7bd42d21a00
Devonte Durham <durhamd@fvtc.edu>
2020-11-28 11:34:40 -0600

    added stuff

--- main.js
+++ main.js
@@ -1,61 +1,79 @@
-class Case { 
-	 
-	constructor(name, amount)  { 
-		 
-		this.name = name; 
-		this.amount = amount; 
-		this.isOpen - false;		
-	 
-	}	
- 
-	showCase()  { 
-	 
-		console.log(`Case Data:\n Case Number: ${this.name}\n Amount: ${this.amount}`); 
-	 
-	}	
- 
-}	
+const lbl1 = document.getElementById('1case')
+
+const dollarAmounts = [.01, 1, 5, 10, 25, 50, 75, 100, 200, 300, 400, 500, 750, 1000, 5000,
+    10000, 25000, 50000, 75000, 100000, 200000, 300000, 400000, 500000, 750000, 1000000]
+
+let shuffledAmounts = null
+let numberZ = []
+
+class Case {
+
+    constructor(name, amount) {
+
+        this.name = name;
+        this.amount = amount;
+        this.isOpen - false;
+
+    }
+
+    showCase() {
+
+        console.log(`Case Data:\n Case Number: ${this.name}\n Amount: ${this.amount}`);
+
+    }
+
+}
 
 class Game {
-	
+
     constructor() {
-		
-	this.playerName = '';
-    this.activeGame = true
-    this.playerWinnings = 0
-    this.bankerOffer = 0
-    this.round = 0
-	this.casesLeft = 26
-	this.prizeValues = [.01, 1, 5, 10, 50, 100, 250, 500, 750, 1000, 3000, 5000, 10000, 15000, 25000, 50000, 75000, 100000, 250000, 500000]
-	}
+
+        this.playerName = '';
+        this.activeGame = true
+        this.playerWinnings = 0
+        this.bankerOffer = 0
+        this.round = 0
+        this.casesLeft = 26
+        this.gameCases = []
+    }
 }
- 
+
+function dollarMe(num) {
+    // Create our number formatter.
+    let formatter = new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD',
+
+        // These options are needed to round to whole numbers if that's what you want.
+        //minimumFractionDigits: 0, // (this suffices for whole numbers, but will print 2500.10 as $2,500.1)
+        //maximumFractionDigits: 0, // (causes 2500.99 to be printed as $2,501)
+    });
+
+    return formatter.format(num);
+}
+
+shuffledAmounts = dollarAmounts.sort(() => Math.random() - 0.5)
+
 // Instantiate an object of type Case(): 
- 
+
 let butts = new Case("1", 1500)
 let g = new Game()
- 
- console.log(g.casesLeft)
-butts.showCase(); 
 
-
-
-
-
-
-
-const game = {
-    activeBoard: true,
-    playerName: 'Player',
-    playerWinnings: 0,
-    offerValue: 0,
-    round: 0,
-    casesToOpen: 26,
-    prizeValues: [.01, 1, 5, 10, 50, 100, 250, 500, 750, 1000, 3000, 5000, 10000, 15000, 25000, 50000, 75000, 100000, 250000, 500000],
-    selectedValues: [0],
+for (let step = 1; step < 27; step++) {
+    // Runs 5 times, with values of step 0 through 4.
+    let newCase = new Case(step, shuffledAmounts[step - 1])
+    g.gameCases.push(newCase)
+    // numberZ.push(step)
 }
 
-const lbl1 = document.getElementById('1case')
+shuffledAmounts.forEach(c => {
+    let newCase = new Case()
+});
+
+
+
+console.log(g.casesLeft)
+butts.showCase();
 
 
 function toggleLabel() {
